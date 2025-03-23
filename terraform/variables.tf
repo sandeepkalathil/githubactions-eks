50d512a0e02e0b7558d8a2c6cc49a2347c003d1c
sandeepkalathil <kalathilsandeepk@gmail.com>
2025-03-23 09:37:43 +0530

    first commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -43,6 +43,16 @@ output "eks_cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
+output "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "eks_cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
 output "eks_cluster_security_group_id" {
   description = "Security group ID attached to the EKS cluster"
   value       = module.eks.cluster_security_group_id
@@ -68,4 +78,18 @@ output "ecr_repository_url" {
   value       = aws_ecr_repository.app_repo.repository_url
 }
 
+output "eks_cluster_role_name" {
+  description = "IAM Role name used for the EKS cluster"
+  value       = module.eks.cluster_iam_role_name
+}
 
+
+output "github_actions_role_name" {
+  description = "IAM Role name used for GitHub Actions"
+  value       = aws_iam_role.github_actions_role.name
+}
+
+output "github_actions_role_arn" {
+  description = "IAM Role ARN used for GitHub Actions"
+  value       = aws_iam_role.github_actions_role.arn
+}
