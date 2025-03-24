6873bec8df4de8800f0ccc6c713545ae0d9d92e3
sandeepkalathil <kalathilsandeepk@gmail.com>
2025-03-24 13:08:01 +0530

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,7 +89,6 @@ resource "aws_ecr_repository" "app_repo" {
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    token                  = module.eks.cluster_auth_token
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   }
 }
